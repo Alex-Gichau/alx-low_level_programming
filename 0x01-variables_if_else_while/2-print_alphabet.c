0214e875a09d12b04e329528d6002a0767112a92
geeshau <gichaumburu@gmail.com>
2022-12-14 05:15:18 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,16 +5,17 @@
  * Description: Get a random number and check its last digit, compare it with 5
  * Return: 0
  */
+
 int main(void)
 {
 	char c = 'a';
 
 	while (c <= 'z')
 	{
-		putchar(c);
+		putchar (c);
 		c++;
 	}
 
-	putchar('\n');
+	putchar ('\n');
 	return (0);
 }
