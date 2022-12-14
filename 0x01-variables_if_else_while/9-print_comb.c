1faaf33603eaae0ed817aa040358d335f8c5da65
Alex-Gichau <gichaumburu@gmail.com>
2022-12-13 19:13:44 -0800

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,20 +3,18 @@
 /**
  * main - main block
  * Description: prints all single digit numbers of base 10
- * starting from 0, followed by a new line.
- * Return: 0
+ * Return 0
  */
 int main(void)
 {
 	int c = 0;
-
-	while (c < 10)
+	while (c < 10 )
 	{
 		putchar(48 + c);
 		if (c != 9)
 		{
 			putchar(',');
-			putchar(' ');
+			putchar('');
 		}
 		c++;
 	}
