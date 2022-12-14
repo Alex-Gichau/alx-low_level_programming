3b896328e39033832e25f3cf0698e2ed65cdb663
Alex-Gichau <gichaumburu@gmail.com>
2022-12-14 13:37:59 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print the alphabet - Print all lowercase
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the audio
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
