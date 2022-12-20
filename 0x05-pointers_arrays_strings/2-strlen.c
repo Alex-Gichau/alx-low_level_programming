b7277fea26cdf47f33c70149ba63f7badc423857
Alex-Gichau <gichaumburu@gmail.com>
2022-12-20 04:14:00 -0800

    2-3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - calculate the length of a string
+ *
+ * @s: the string to calculate it's length
+ * Return: Length of a string
+ */
+
+int _strlen(har *s)
+{
+	int i;
+	int length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+	return (length);
+}
