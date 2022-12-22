860ce954543c7a1d829bf0f977f4fc103a8c0fcd
Alex-Gichau <gichaumburu@gmail.com>
2022-12-22 00:49:41 -0800

    1-strncat.c created

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - function tat concatenates two strings
+ * @src: copy
+ * @dest: destination
+ * @n: number
+ * Return: destination return
+ */
+
+char *_strncat(char *dest, char, char *src, int n)
+{
+	int index = strlen(dest);
+	int a = 0;
+
+	while (a < n && *src)
+	{
+		dest[index + a] = *src;
+		src++;
+		a++;
+	}
+	dest[index + a] = '\0';
+	return (dest);
+}
