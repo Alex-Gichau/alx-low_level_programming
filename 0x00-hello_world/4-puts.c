76d0fe0f62e200da30a1d4e7448aa8e367497326
Alex Gichau <gichaumburu@gmail.com>
2023-05-22 18:02:18 -0700

    101-quote.c add

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,6 @@
 
 int main(void)
 {
-	char Str = "\"Programming is like building a multilingual puzzle";
-
-	puts(Str);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
