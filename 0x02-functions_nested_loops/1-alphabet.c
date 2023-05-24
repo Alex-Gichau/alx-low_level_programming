cce12498ef1d115b816c2859b9b51ee017d6a28b
Alex Gichau <gichaumburu@gmail.com>
2023-05-24 09:13:46 -0700

    main.h complete

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char l = 'a';
+
+	while (l <= 'z')
+	{
+		putchar ("%c\n", l);
+		l++;
+		return (0);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,18 @@
-#include <stdio.h>
+#include "11-print_to_98.c"
 #include "0-putchar.c"
-
-int main(void)
-{
-	0-putchar();
-}
+#include "1-alphabet.c"
+#include "2-print_alphabet_x10.c"
+#include "3-islower.c"
+#include "4-isalpha.c"
+#include "5-sign.c"
+#include "6-abs.c"
+#include "7-print_last_digit.c"
+#include "8-24_hours.c"
+#include "9-times_table.c"
+#include "10-add.c"
+#include "11-print_to_98.c"
+#include "100-times_table.c"
+#include "101-natural.c"
+#include "102-fibonacci.c"
+#include "103-fibonacci.c"
+#include "104-fibonacci.c"
