b50f88498f13a756cd9dbc651114483a6285e649
Alex Gichau <gichaumburu@gmail.com>
2023-06-02 06:41:18 -0700

    2-strncpy.c changed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,10 +2,11 @@
 
 /**
  * _strcmp - comparing the values
- * @s1: input data
- * @s2: input data
+ * @s1: input value
+ * @s2: input value
  * Return: s1[i] - s2[i]
  */
+
 int _strcmp(char *s1, char *s2)
 {
 	int i;
@@ -13,7 +14,7 @@ int _strcmp(char *s1, char *s2)
 	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[i] != s[2])
+		if (s1[i] != s2[i])
 		{
 			return (s1[i] - s2[i]);
 		}
