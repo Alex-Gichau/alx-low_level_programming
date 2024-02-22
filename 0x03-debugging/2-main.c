bbb8c38dc1afb30efa8ba2708738ab204874f1ab
Alex Gichau <gichaumburu@gmail.com>
2024-02-22 11:28:27 -0800

    2-largest_number.c added

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -8,16 +8,18 @@
 
 int main(void)
 {
-        int a, b, c;
-        int largest;
+	int a, b, c;
+	int largest;
 
-        a = 972;
-        b = -98;
-        c = 0;
+	int largest_number(int a, int b, int c);
 
-        largest = largest_number(a, b, c);
+	a = 972;
+	b = -98;
+	c = 0;
 
-        printf("%d is the largest number\n", largest);
+	largest = largest_number(a, b, c);
 
-        return (0);
+	printf("%d is the largest number\n", largest);
+
+	return (0);
 }
