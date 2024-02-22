8005973835b58b46b4baa906c39ebe595b35678b
Alex Gichau <gichaumburu@gmail.com>
2024-02-21 22:12:54 -0800

    1-main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 
 	i = 0;
 
-	/* while (i < 10)
-	{
-		putchar(i);
-	} do not delete! */
+	/* while (i < 10) */
+	/* { */
+	/*	putchar(i); */
+	/* }  */
 
 	printf("Infinite loop avoided! \\o/\n");
 
